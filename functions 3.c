66b314b76f19927d7eafbae9541cbec9ebd06774
Mydhili2009 <25kq1a5472@pace.ac.in>
2025-11-26 20:50:38 +0530

    Add files via upload

---
+++ functions 3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int prime( int n);
+int main(){
+int n;
+printf("enter number");
+scanf("%d",&n);
+if(prime(n))
+printf("%d is a prime number",n);
+else
+printf("%d is a prime number\n",n);
+}
+int prime(int n){
+if(n<=1)
+return 0;
+for(int i=2;i<=n/2;i++){
+if(n%i==0)
+return 0;
+}
+return 1;
+}
+
+
+
